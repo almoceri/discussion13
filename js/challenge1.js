fe612d368ee99299e75f5540c99c4c5d1ecaa74e
Alfonso Moceri <156968542+almoceri@users.noreply.github.com>
2024-04-10 18:46:03 -0400

    Add files via upload

---
+++ js/challenge1.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function() {
+    this.document.querySelector("h1").className = "heading";
+
+
+})
+
+
+window.addEventListener("dblclick", function() {
+    this.alert(new Date());
+
+
+})
